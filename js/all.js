527cc56776d2e7965e23fd321fdc7d4f6359b598
TFD10506 <TFD10506@gmail.com>
2022-02-16 21:12:58 +0800

    RWD_漢堡

--- js/all.js
+++ js/all.js
@@ -1,5 +1,20 @@
 //有時間再改掉
 AOS.init();
+//漢堡選單
+$('#ham-icon').click(function () {
+    $(this).toggleClass('open');
+});
+// 點擊按鈕，選單縮放
+$("#ham-icon").on("click", function () {
+    $(".ham-nav").slideToggle("slow");
+});
+$("#ham-menu-text").on("click", function () {
+    $(this).children(".ham-submenu").slideToggle("slow", "linear", change());
+});
+function change() {
+    let icon = document.getElementsByClassName("fa-angle-down")[0];
+    icon.classList.toggle("-change");
+}
 // 滾輪判斷取消
 // window.onscroll = function () {
 //     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
