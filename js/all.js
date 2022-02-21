261bfab9451cc8beaabb6eed71bd8f4733f85a41
TFD10506 <TFD10506@gmail.com>
2022-02-21 21:48:52 +0800

    aos動畫改成手刻(淚

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 //有時間再改掉
-AOS.init();
+// AOS.init();
 //漢堡選單
 $('#ham-icon').click(function () {
     $(this).toggleClass('open');
@@ -28,3 +28,50 @@ function change() {
 //         // navBg.style.boxShadow = "none";
 //     }
 // }
+
+//降低滾動判斷次數
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+
+    return function () {
+        var context = this, args = arguments;
+
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+
+        if (callNow) func.apply(context, args);
+    };
+};
+
+//滾動效果
+let sliderItems = document.querySelectorAll('.slide-in');
+// console.log("sliderItems:"+sliderItems);
+function checkSlide(e) {
+    sliderItems.forEach(function(sliderItem) {
+        // 取得sliderItems 1/2高度的定位點（卷軸垂直位移量＋視窗高度）- 1/4 sliderItems高度
+        let slideInAt = (window.scrollY + window.innerHeight) - sliderItem.offsetHeight / 4;
+        // console.log("slideInAt:"+slideInAt);
+        // 取得sliderItems底部定位點（利用sliderItems頂部定位點+sliderItems高度取得）
+        let imageBottom = sliderItem.offsetTop + sliderItem.offsetHeight;
+        // console.log("imageBottom:"+imageBottom);
+        // 判斷視窗是否已經超過sliderItems高度一半
+        let isHalfShown = slideInAt > sliderItem.offsetTop;
+        // console.log("isHalfShown:"+isHalfShown);
+        // 判斷滾動範圍是否已經超過sliderItems底部（卷軸垂直位移量）
+        let isNotScrolledPast = window.scrollY < imageBottom;
+        // console.log("isNotScrolledPast:"+isNotScrolledPast);
+        // 判斷是否超過sliderItems一半高，且視窗尚未超過sliderItems底部來增加或移除css效果
+        if (!(isHalfShown && isNotScrolledPast)) {
+            sliderItem.classList.remove("-scroll_active");
+        } else {
+            sliderItem.classList.add("-scroll_active");
+        }
+    });
+}
+window.addEventListener('scroll', debounce(checkSlide));
