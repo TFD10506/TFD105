092d46b53b914b4babe88498598ca73d342670b3
TFD10506 <TFD10506@gmail.com>
2022-02-22 16:51:09 +0800

    top按鈕新增

--- js/all.js
+++ js/all.js
@@ -29,6 +29,7 @@ function change() {
 //     }
 // }
 
+// -----動畫效果區SRART-----
 //降低滾動判斷次數
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
@@ -48,24 +49,18 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 };
-
 //滾動效果
 let sliderItems = document.querySelectorAll('.slide-in');
-// console.log("sliderItems:"+sliderItems);
 function checkSlide(e) {
-    sliderItems.forEach(function(sliderItem) {
+    sliderItems.forEach(function (sliderItem) {
         // 取得sliderItems 1/2高度的定位點（卷軸垂直位移量＋視窗高度）- 1/4 sliderItems高度
         let slideInAt = (window.scrollY + window.innerHeight) - sliderItem.offsetHeight / 4;
-        // console.log("slideInAt:"+slideInAt);
         // 取得sliderItems底部定位點（利用sliderItems頂部定位點+sliderItems高度取得）
         let imageBottom = sliderItem.offsetTop + sliderItem.offsetHeight;
-        // console.log("imageBottom:"+imageBottom);
         // 判斷視窗是否已經超過sliderItems高度一半
         let isHalfShown = slideInAt > sliderItem.offsetTop;
-        // console.log("isHalfShown:"+isHalfShown);
         // 判斷滾動範圍是否已經超過sliderItems底部（卷軸垂直位移量）
         let isNotScrolledPast = window.scrollY < imageBottom;
-        // console.log("isNotScrolledPast:"+isNotScrolledPast);
         // 判斷是否超過sliderItems一半高，且視窗尚未超過sliderItems底部來增加或移除css效果
         if (!(isHalfShown && isNotScrolledPast)) {
             sliderItem.classList.remove("-scroll_active");
@@ -75,3 +70,26 @@ function checkSlide(e) {
     });
 }
 window.addEventListener('scroll', debounce(checkSlide));
+// -----動畫效果區END-----
+
+// -----TOP按鈕START-----
+//top按鈕
+var topbutton = document.getElementById("topBtn");
+
+//scroll
+window.onscroll = function () { scrollFunction() };
+//判斷超過20
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topbutton.style.display = "block";
+    } else {
+        topbutton.style.display = "none";
+    }
+}
+
+// 點擊回到頂端
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// -----TOP按鈕END-----
