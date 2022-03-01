bc385b6db945fdce6e69ccae2e9702a0babe06ba
TFD10506 <TFD10506@gmail.com>
2022-03-01 13:37:42 +0800

    [add]增加message測試

--- js/all.js
+++ js/all.js
@@ -110,3 +110,20 @@ function topFunction() {
     $('html, body').animate({ scrollTop: 0 }, 700);
 }
 // -----TOP按鈕END-----
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "101336992507672");
+chatbox.setAttribute("attribution", "biz_inbox");
+window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v13.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/zh_TW/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
